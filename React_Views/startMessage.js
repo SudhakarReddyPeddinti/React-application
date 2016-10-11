8d91ebac1bc885890b096a6e123279128d98a600
SudhakarReddyPeddinti <p.reddy.sudhakar@gmail.com>
2016-10-10 21:41:12 -0500

    Removing redundant files

--- React_Views/startMessage.js
+++
@@ -1,6 +0,0 @@
-import colors from 'colors';
-
-/*eslint-disable no-console */
-
-console.log('Starting app in development mode, running tests, linting, and opening default browser at http://localhost:3000...'.green);
-
